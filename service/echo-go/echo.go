b97ea611fd22f7961fe9d9c049c1c738258408d8
Itamar Ostricher <itamarost@gmail.com>
2017-07-25 19:41:43 +0300

    Go implementation of simple echo server

---
+++ service/echo-go/echo.go
@@ -0,0 +1,23 @@
+// -*- coding: utf-8 -*-
+// Copyright 2017 Itamar Ostricher
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := flag.String("port", "8900", "Server port to listen on")
+	flag.Parse()
+	log.Println("starting server, listening on port " + *port)
+	http.HandleFunc("/", EchoHandler)
+	http.ListenAndServe(":" + *port, nil)
+}
+
+func EchoHandler(w http.ResponseWriter, request *http.Request) {
+	log.Printf("Echoing back request made to %s to client (%s)", request.URL.Path, request.RemoteAddr)
+	request.Write(w)
+}
